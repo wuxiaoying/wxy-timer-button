4f5169f5cbc7cb767108ad8588465fd8314c3c39
helenxywu <xiaoyingw12@gmail.com>
2015-01-21 21:03:54 -0800

    initial

--- wxy-timer-button.coffee
+++ wxy-timer-button.coffee
@@ -1,3 +1,37 @@
 Polymer
-  ready: ->
+  raised: true
+  z: 3
+
+  eventDelegates:
+    tap: 'onTap'
+
+  attached: ->
+    @width = @clientWidth
+    @radius = @width / 2 - 5
+    @center = @width / 2
+    return
+
+  onTap: ->
+    if @raised
+      @raised = false
+      @$.shadow.setZ 0
+
+    @_StartTimer()
+    return
+
+  reset: ->
+    @raised = true
+    @$.shadow.setZ 3
+    return
+
+  _StartTimer: ->
+    if @time > 0
+      @job 'time', ->
+        @time--
+        @_StartTimer()
+        return
+      , 1000
+    else
+      @fire 'complete'
+
     return
--- wxy-timer-button.js
+++ wxy-timer-button.js
@@ -1,6 +1,36 @@
 (function() {
   Polymer({
-    ready: function() {}
+    raised: true,
+    z: 3,
+    eventDelegates: {
+      tap: 'onTap'
+    },
+    attached: function() {
+      this.width = this.clientWidth;
+      this.radius = this.width / 2 - 5;
+      this.center = this.width / 2;
+    },
+    onTap: function() {
+      if (this.raised) {
+        this.raised = false;
+        this.$.shadow.setZ(0);
+      }
+      this._StartTimer();
+    },
+    reset: function() {
+      this.raised = true;
+      this.$.shadow.setZ(3);
+    },
+    _StartTimer: function() {
+      if (this.time > 0) {
+        this.job('time', function() {
+          this.time--;
+          this._StartTimer();
+        }, 1000);
+      } else {
+        this.fire('complete');
+      }
+    }
   });
 
 }).call(this);
